d31ec395936a2279fe30fd24daed0c5fdd9dd444
vattu <venkateshvatturi2092@gmail.com>
2023-05-04 19:53:40 -0500

    Vatturi Initial Commit

---
+++ Chat GPT/Controllers/GPTController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API.Completions;
+using OpenAI_API;
+
+namespace Chat_GPT.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class GPTController : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<IActionResult> GetData(string input)
+        {
+
+            Console.WriteLine("Method Entered!");
+            string apikey = "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";
+            string response = "";
+            OpenAIAPI gptcontroller = new OpenAIAPI(apikey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = input;
+            completion.Model = "text-davinci-003";
+            completion.MaxTokens = 4000;
+            var output = await gptcontroller.Completions.CreateCompletionsAsync(completion);
+            if (output != null)
+            {
+                foreach (var item in output.Completions)
+                {
+                    response = item.Text;
+
+                }
+                return Ok(response);
+
+            }
+            else
+            { 
+                return BadRequest("NotFound");
+            }
+        }
+    }
+}
